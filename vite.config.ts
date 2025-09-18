06d996b0d115d4d803a17848f0f579406104fecc
hungdonguyen <167590894+hungdonguyen@users.noreply.github.com>
2025-09-18 11:30:50 +0700

    FIx logo, cho phép truy cập qua điện thoại

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Cho phép truy cập từ mạng ngoài
+    port: 5173, // Cổng mặc định
+    strictPort: false, // Tự động chọn cổng khác nếu 5173 bị chiếm
+  }
 })
